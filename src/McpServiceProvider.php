6f5df6effeec1f3e367a0f77f3961316d372ba36
Dewald Hugo <dewaldhugo@gmail.com>
2026-06-14 07:44:31 +0200

    chore: bind ToolRegistry as singleton and wire up ListRoutes implementation

--- src/McpServiceProvider.php
+++ src/McpServiceProvider.php
@@ -4,6 +4,8 @@ namespace OriginMain\LaravelMcp;
 
 use Illuminate\Support\ServiceProvider;
 use OriginMain\LaravelMcp\Commands\McpServeCommand;
+use OriginMain\LaravelMcp\Services\ToolRegistry;
+use OriginMain\LaravelMcp\Services\Tools\ListRoutes;
 
 class McpServiceProvider extends ServiceProvider
 {
@@ -12,7 +14,14 @@ class McpServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(ToolRegistry::class, function () {
+            $registry = new ToolRegistry();
+            
+            // Register tools for compilation here
+            $registry->register(new ListRoutes());
+            
+            return $registry;
+        });
     }
 
     /**
